e4855c6f621511b7437f696de2b6cf92c006a74f
smnsprnsktt <simona.sparnauskaite@adform.com>
2020-12-21 19:10:31 +0200

    add lecture

---
+++ 11paskaita/main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let arrayVar = [true, "Simona", 123];
+
+let objectVar = { name: "Simona", age: 25 };
+
+function funcTest(num) {
+  return num + 1;
+}
+
+console.log(arrayVar);
+console.log(objectVar);
+console.log(funcTest(4));
