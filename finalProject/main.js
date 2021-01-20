cc1f975fb0c8f7b76f1f45bb093c4d60b3897562
smnsprnsktt <simona.sparnauskaite@adform.com>
2021-01-21 01:08:18 +0200

    adding some style

--- finalProject/main.js
+++ finalProject/main.js
@@ -2,25 +2,20 @@
 
 let info = [];
 let pointsCount = 0;
-const totalRounds = 15;
+let totalRounds = 0;
 let roundsPlayed = 0;
 
 const flagImage = document.createElement("img");
-
 const optionButton1 = document.createElement("button"); // teisingas atsakymas
 const optionButton2 = document.createElement("button");
 const optionButton3 = document.createElement("button");
 const finishModal = document.createElement("div");
 const modalContent = document.createElement("div");
 
-// function getRandomNumber() {
-//   return Math.floor(Math.random() * 250);
-// }
-
 function generateRandomNumbers() {
-  const nums = new Set();
+  const nums = new Set(); // setas nesikartojanciu skaiciu
   while (nums.size !== 3) {
-    nums.add(Math.floor(Math.random() * 250) + 1);
+    nums.add(Math.floor(Math.random() * 250));
   }
   return [...nums];
 }
@@ -31,18 +26,24 @@ function loadNewFlag() {
       getData();
     }, 1000);
   } else {
-    let modal = document.querySelector(".modal");
-    modal.style.display = "block";
-    // finishModal.className = "modal fade show";
-    // modalContent.className = "modal-content";
-    // modalContent.innerHTML = "div";
-    // body.appendChild(finishModal);
-    // finishModal.appendChild(modalContent);
+    $("#exampleModalCenter").modal({});
+    let modalMessage = document.querySelector("#modal-message");
+
+    modalMessage.innerHTML = `${pointsCount} correct out of ${totalRounds}`;
   }
-
   roundsPlayed = roundsPlayed + 1;
-
   console.log(roundsPlayed);
+}
+
+function startNewGame(numberOfRounds) {
+  roundsPlayed = 0;
+  pointsCount = 0;
+  totalRounds = numberOfRounds;
+  let score = document.querySelector("#score");
+  score.innerHTML = `${pointsCount} correct out of  ${totalRounds} ${
+    pointsCount > 0 ? "😮" : "😅"
+  } `;
+  getData();
 }
 
 function changeButtonState(state) {
@@ -52,50 +53,57 @@ function changeButtonState(state) {
 }
 
 function getData() {
-  let container = document.querySelector("#container");
   let choices = document.querySelector("#choices");
   let score = document.querySelector("#score");
   let roundNum = document.querySelector("#roundNumber");
-  let rounds = document.querySelector("#rounds");
-  let startButton = document.getElementById("startButton");
-  startButton.style.display = "none";
+  document.getElementById("startButton5").style.display = "none";
+  document.getElementById("startButton10").style.display = "none";
+  document.getElementById("startButton15").style.display = "none";
+  document.getElementById("startButton20").style.display = "none";
+  document.getElementById("container").style.marginTop = 0;
+  document.getElementById("roundsQuestion").style.display = "none";
 
   fetch("https://restcountries.eu/rest/v2/all")
     .then((response) => response.json())
     .then((data) => {
       info = data;
-      const randomNumbers = generateRandomNumbers(); // sugeneruojame random skaicius
-      roundNum.innerHTML = roundsPlayed + 1;
+      const randomNumbers = generateRandomNumbers(); // sugeneruoj random skaicius
+      roundNum.innerHTML = `Round ${roundsPlayed + 1}`;
       flagImage.src = data[randomNumbers[0]].flag; // atitinkama flag
-      flagImage.style.width = "400px";
+      flagImage.style.height = "250px";
       optionButton1.innerHTML = data[randomNumbers[0]].name; // sugeneruojamas teisingas salies name atsakymas
       optionButton2.innerHTML = data[randomNumbers[1]].name; // nauja random salis incorrect
       optionButton3.innerHTML = data[randomNumbers[2]].name; // nauja random salis incorrect
 
-      optionButton1.style.backgroundColor = "rgb(125, 240, 205)";
-      optionButton2.style.backgroundColor = "rgb(125, 240, 205)";
-      optionButton3.style.backgroundColor = "rgb(125, 240, 205)";
+      optionButton1.className = "btn btn-light";
+      optionButton2.className = "btn btn-light";
+      optionButton3.className = "btn btn-light";
+
+      optionButton1.style.backgroundColor = "#f8f9fa";
+      optionButton2.style.backgroundColor = "#f8f9fa";
+      optionButton3.style.backgroundColor = "#f8f9fa";
       changeButtonState(false);
       optionButton1.onclick = function () {
-        optionButton1.style.backgroundColor = "green";
+        optionButton1.style.backgroundColor = "#20c960";
         pointsCount = pointsCount + 1;
-        score.innerHTML = `${pointsCount} correct out of `;
-        rounds.innerHTML = totalRounds;
+        score.innerHTML = `${pointsCount} correct out of  ${totalRounds} ${
+          pointsCount > 0 ? "😮" : "😅"
+        } `;
         changeButtonState(true);
         loadNewFlag();
       };
       optionButton2.onclick = function () {
-        optionButton2.style.backgroundColor = "red";
+        optionButton2.style.backgroundColor = "#c91414";
         changeButtonState(true);
         loadNewFlag();
       };
       optionButton3.onclick = function () {
-        optionButton3.style.backgroundColor = "red";
+        optionButton3.style.backgroundColor = "#c91414";
         changeButtonState(true);
         loadNewFlag();
       };
 
-      container.appendChild(flagImage);
+      flagsContainer.appendChild(flagImage);
       choices.appendChild(optionButton1);
       choices.appendChild(optionButton2);
       choices.appendChild(optionButton3);
